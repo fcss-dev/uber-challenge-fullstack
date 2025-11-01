6a5dc9a93350e337453bcf32f3ac40a93e8ef4e7
FranciscoCleiton-Dev <fccleitonfs.329@gmail.com>
2025-11-01 20:20:17 -0300

    camada application
    implementação dos casos de uso

---
+++ src/main/java/fcss_dev/uber/email_service/application/EmailSenderService.java
@@ -0,0 +1,20 @@
+package fcss_dev.uber.email_service.application;
+
+import fcss_dev.uber.email_service.adapters.EmailSenderGateway;
+import fcss_dev.uber.email_service.core.EmailSenderUserCase;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EmailSenderService implements EmailSenderUserCase {
+
+    private final EmailSenderGateway emailSenderGateway;
+
+    @Autowired
+    public EmailSenderService(EmailSenderGateway emailGateway){
+        this.emailSenderGateway = emailGateway;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        this.emailSenderGateway.sendEmail(to, subject, body);
+    }
+}
