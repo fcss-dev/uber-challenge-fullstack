257f047ce258decb13f9e0410945d562047984ce
FranciscoCleiton-Dev <fccleitonfs.329@gmail.com>
2025-11-01 21:00:37 -0300

    anotação service

--- src/main/java/fcss_dev/uber/email_service/application/EmailSenderService.java
+++ src/main/java/fcss_dev/uber/email_service/application/EmailSenderService.java
@@ -3,7 +3,9 @@ package fcss_dev.uber.email_service.application;
 import fcss_dev.uber.email_service.adapters.EmailSenderGateway;
 import fcss_dev.uber.email_service.core.EmailSenderUserCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailSenderService implements EmailSenderUserCase {
 
     private final EmailSenderGateway emailSenderGateway;
