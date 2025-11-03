fea30455e5d175f44bcbd993fddc9082923a84d9
FranciscoCleiton-Dev <fccleitonfs.329@gmail.com>
2025-11-03 13:43:30 -0300

    implementar serviço SES para envio de e-mails

---
+++ src/main/java/fcss_dev/uber/email_service/infra/ses/SesEmailSender.java
@@ -0,0 +1,36 @@
+package fcss_dev.uber.email_service.infra.ses;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.*;
+import fcss_dev.uber.email_service.adapters.EmailSenderGateway;
+import fcss_dev.uber.email_service.core.exceptions.EmailServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SesEmailSender implements EmailSenderGateway {
+
+    private final AmazonSimpleEmailService amazonSimpleEmailService;
+
+    @Autowired
+    public SesEmailSender(AmazonSimpleEmailService amazonSimpleEmailService){
+        this.amazonSimpleEmailService = amazonSimpleEmailService;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        SendEmailRequest request = new SendEmailRequest()
+                .withSource("remetentetest@gmail.com") // remetente
+                .withDestination(new Destination().withToAddresses(to)) // to - destinatario
+                .withMessage(new Message()
+                        .withSubject(new Content(subject))
+                        .withBody(new Body().withText(new Content(body)))
+                );
+        try {
+            this.amazonSimpleEmailService.sendEmail(request);
+        } catch (AmazonServiceException e) {
+            throw new EmailServiceException("Falha ao enviar e-mail", e);
+        }
+    }
+}
