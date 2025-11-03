07f806d0855d9f38de28f4a12a2ffb5301f544ea
FranciscoCleiton-Dev <fccleitonfs.329@gmail.com>
2025-11-03 13:42:49 -0300

    adaptação da configuration para funcionamento da aplicação

---
+++ src/main/java/fcss_dev/uber/email_service/infra/ses/AwsSesConfig.java
@@ -0,0 +1,31 @@
+package fcss_dev.uber.email_service.infra.ses;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsSesConfig {
+
+    @Value("${aws.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public AmazonSimpleEmailService amazonSimpleEmailService(){
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKeyId, secretKey);
+        return AmazonSimpleEmailServiceClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .withRegion(region)
+                .build();
+    }
+}
